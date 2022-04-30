1750fffb07f07419e1633ba1fb151a06a9aa8353
LuanSilveiraSouza <luansouzasilveira@gmail.com>
2022-04-30 11:47:46 -0300

    feat: start main page design

--- app/src/index.js
+++ app/src/index.js
@@ -5,5 +5,9 @@ const index = document.querySelector("div#articles");
 console.log(index);
 
 Object.values(articles).forEach((article) => {
-	index.innerHTML += `<div><a href="./${article.name}.html">${article.title}</a></div>`;
+	index.innerHTML += 
+	`<div class="w-auto h-40 border-black-100 border-2 rounded-md">
+		<a href="./${article.name}.html">${article.title}</a>
+	</div>
+	`;
 });
